7a8b2c3942f07afdf65c8bbbf1d64430e69dda24
wstjf666 <141121346+wstjf666@users.noreply.github.com>
2023-08-07 14:26:31 +0000

    init

--- init_repo.sh
+++ init_repo.sh
@@ -1,8 +1,10 @@
 tagg='android13-5.15-2022-11'
+sed --version
+/usr/bin/sed --version
 ../git-repo/repo init --depth=1 --u https://android.googlesource.com/kernel/manifest -b common-$tagg
 REMOTE_BRANCH=$(git ls-remote https://android.googlesource.com/kernel/common $tagg)
 if grep -q deprecated <<< $REMOTE_BRANCH; then
     echo "Found deprecated branch: $tagg"
-    sed -i 's/"$tagg"/"deprecated\/$tagg"/g' .repo/manifests/default.xml
+    /usr/bin/sed -i 's/"$tagg"/"deprecated\/$tagg"/g' .repo/manifests/default.xml
     cat .repo/manifests/default.xml
 fi
